f8be039c905d295bcb2cf620b0752de2e2c75e88
BestVersion7 <hunterkfan@gmail.com>
2023-06-21 17:47:30 -0400

    data

--- app/test/page.js
+++ app/test/page.js
@@ -1,12 +1,12 @@
 const getComments = async () => {
     const data2 = await fetch(
-        `https://hunterkf.com/api/comment&API_KEY${process.env.API_KEY}`
+        `https://hunterkf.com/api/comment&API_KEY=${process.env.API_KEY}`
     );
     const data = await data2.json();
     return data;
 };
 
 export default function TestPage() {
-    console.log(data);
+    console.log('data');
     return <div>This is a test Page fetch</div>;
 }
--- components/test/AddForm.js
+++ components/test/AddForm.js
@@ -1,7 +1,24 @@
 "use client";
 
+// const handleForm = async (comment) => {
+//     const data2 = await fetch(
+//         `https://hunterkf.com/api/comment&API_KEY=${process.env.API_KEY}`,
+//         { headers: { "Content-Type": "application/json" } }
+//     );
+// };
+
 const AddForm = () => {
-    return <form></form>;
+    const handleSubmit = e => {
+        console.log('f')
+    }
+    return (
+        <form>
+            <textarea name="s" id="4" cols="30" rows="2"></textarea>
+            <button onClick={handleSubmit} type="submit">
+                Submit
+            </button>
+        </form>
+    );
 };
 
 export default AddForm;
