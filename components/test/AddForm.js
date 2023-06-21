501922c5326d23731d947f526d13133a4515ad0c
BestVersion7 <hunterkfan@gmail.com>
2023-06-21 19:53:22 -0400

    add cors to nextconfig

--- components/test/AddForm.js
+++ components/test/AddForm.js
@@ -1,39 +1,41 @@
 "use client";
 
-import { useRef } from "react";
+// import { useRef } from "react";
 // import { useRouter } from "next/router";
 
-const url2 = `https://hunterkf.com/api/comment?API_KEY=${process.env.API_KEY}`;
+const url2 = `https://hunterkf.com/api/comment?API_KEY=`;
 
 async function postData(url = "", data = {}) {
-    const data2 = await fetch(url, {
-        method: "POST",
-        // mode: "cors",
-        headers: {
-            "Content-Type": "application/json",
-        },
+    // const data2 = await fetch(url, {
+    //     method: "POST",
+    //     // mode: "cors",
+    //     // headers: {
+    //     //     "Content-Type": "application/json",
+    //     // },
 
-        body: "fdslkj"
-    });
+    //     body: JSON.stringify({ comment_body: "fdslkj" }),
+    // });
+    const data2 = await fetch(url2);
     const data3 = await data2.json();
+    console.log(data3)
     return res.json(data3);
 }
 
 const AddForm = () => {
-    const inputRef = useRef();
+    // const inputRef = useRef();
 
     const handleSubmit = async (e) => {
         await postData(url2);
     };
     return (
         <form onSubmit={(e) => e.preventDefault()}>
-            <textarea
+            {/* <textarea
                 ref={inputRef}
                 name="s"
                 id="4"
                 cols="30"
                 rows="2"
-            ></textarea>
+            ></textarea> */}
             <button onClick={handleSubmit} type="submit">
                 Submit
             </button>
--- middleware.js
+++ middleware.js
@@ -10,5 +10,5 @@ export async function middleware(req) {
 }
 
 export const config = {
-    matcher: "/apis/:path*",
+    matcher: "/api/:path*",
 };
