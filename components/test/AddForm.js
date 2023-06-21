dc724981b893c7673f36959f1442acffa66a9af4
BestVersion7 <hunterkfan@gmail.com>
2023-06-21 19:34:17 -0400

    update middleware test

--- components/test/AddForm.js
+++ components/test/AddForm.js
@@ -3,17 +3,17 @@
 import { useRef } from "react";
 // import { useRouter } from "next/router";
 
-// const url2 = `https://hunterkf.com/api/comment?API_KEY=${process.env.API_KEY}`;
+const url2 = `https://hunterkf.com/api/comment?API_KEY=${process.env.API_KEY}`;
 
 async function postData(url = "", data = {}) {
-    const data2 = await fetch({
+    const data2 = await fetch(url, {
         method: "POST",
-        mode: "cors",
+        // mode: "cors",
         headers: {
             "Content-Type": "application/json",
         },
 
-        body: JSON.stringify({ comment_body: "fdsl" }),
+        body: "fdslkj"
     });
     const data3 = await data2.json();
     return res.json(data3);
@@ -23,7 +23,7 @@ const AddForm = () => {
     const inputRef = useRef();
 
     const handleSubmit = async (e) => {
-        await postData();
+        await postData(url2);
     };
     return (
         <form onSubmit={(e) => e.preventDefault()}>
--- middleware.js
+++ middleware.js
@@ -1,11 +1,19 @@
 import { NextResponse } from "next/server";
 
+export const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
 // This function can be marked `async` if using `await` inside
 export async function middleware(req) {
     if (req.nextUrl.searchParams.get("API_KEY") !== process.env.API_KEY) {
         return NextResponse.json("fail", { status: 401 });
     }
-
+    if (req.nextUrl.pathname.startsWith("/api")) {
+        // NextResponse.next().headers.append("Access-Control-Allow-Origin", "*");
+        NextResponse.json({}, { headers: corsHeaders });
+    }
     return NextResponse.next();
 }
 
