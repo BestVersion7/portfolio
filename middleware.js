f9bc0aee55496217333bd0833bd3f91fd538d8b3
BestVersion7 <hunterkfan@gmail.com>
2023-06-21 19:37:03 -0400

    stop middleware

--- middleware.js
+++ middleware.js
@@ -1,22 +1,14 @@
 import { NextResponse } from "next/server";
 
-export const corsHeaders = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type, Authorization",
-};
 // This function can be marked `async` if using `await` inside
 export async function middleware(req) {
     if (req.nextUrl.searchParams.get("API_KEY") !== process.env.API_KEY) {
         return NextResponse.json("fail", { status: 401 });
     }
-    if (req.nextUrl.pathname.startsWith("/api")) {
-        // NextResponse.next().headers.append("Access-Control-Allow-Origin", "*");
-        NextResponse.json({}, { headers: corsHeaders });
-    }
+
     return NextResponse.next();
 }
 
 export const config = {
-    matcher: "/api/:path*",
+    matcher: "/apis/:path*",
 };
