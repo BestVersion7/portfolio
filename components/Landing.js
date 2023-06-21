a9f1b438d6cac70c2336b5f9ef75e7ec7402525b
BestVersion7 <hunterkfan@gmail.com>
2023-06-21 16:09:26 -0400

    wrap landing

--- components/Landing.js
+++ components/Landing.js
@@ -7,7 +7,7 @@ const Landing = () => {
     return (
         <div style={{ position: "relative" }} className="section-home">
             <div className="section-home-name">
-                <h2>Hi I'm Hunter</h2>
+                <h2>{`Hi I'm Hunter`}</h2>
                 <h2>Welcome to my site! </h2>
             </div>
             <button className="icon-down" onClick={handleScrollDown}>
