fe42f9a0f5d2f0298438416c22d12b2a569844be
BestVersion7 <hunterkfan@gmail.com>
2023-06-21 22:05:42 -0400

    update route

--- app/api/comment/route.js
+++ app/api/comment/route.js
@@ -11,7 +11,12 @@ export async function GET() {
         },
         take: 10,
     });
-    return NextResponse.json(data);
+    return new NextResponse(JSON.stringify(data), {
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json",
+        },
+    });
 }
 
 export async function POST(req) {
@@ -25,5 +30,11 @@ export async function POST(req) {
         },
     });
 
-    return NextResponse.json(data, { status: 201 });
+    return new NextResponse(JSON.stringify(data), {
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json",
+        },
+        status: 201,
+    });
 }
