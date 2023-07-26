ef88966f509462916b0c902146a478200dcaa82d
BestVersion7 <hunterkfan@gmail.com>
2023-07-26 13:05:51 -0400

    updated booking

--- app/api/booking/route.ts
+++ app/api/booking/route.ts
@@ -1,9 +1,19 @@
 import { NextResponse, NextRequest } from "next/server";
 import { prisma } from "@/lib/prisma";
 
-export async function GET() {
+export async function GET(req: NextRequest) {
     try {
-        const data = await prisma.booking.findMany();
+        let data;
+        const booking_date = req.nextUrl.searchParams.get("booking_date");
+        if (booking_date !== null) {
+            await prisma.booking.findMany({
+                where: {
+                    booking_date: booking_date,
+                },
+            });
+        } else {
+            data = await prisma.booking.findMany();
+        }
         return NextResponse.json(data);
     } catch (error) {
         return NextResponse.json("error", { status: 500 });
