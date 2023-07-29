fceda9dc95edf9682c8f4cf5a62892210077557f
BestVersion7 <hunterkfan@gmail.com>
2023-07-28 23:31:18 -0400

    not

--- app/not-found.js
+++ app/not-found.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function NotFound() {
     return (
         <div>
-            <Link href="/">Home</Link>
+            <Link href="/">Back</Link>
             <h2>Not Found</h2>
             <p>Could not find requested resource</p>
         </div>
