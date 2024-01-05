0a9f23b39e6587ae006b93f69e6b359a4a2dd728
BestVersion7 <hunterkfan@gmail.com>
2024-01-05 12:18:38 -0500

    edit middleware for origin

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,41 @@
 import { NextRequest, NextResponse } from "next/server";
 
+let allowedOrigins: string[] = [];
+process.env.NODE_ENV === "production"
+  ? (allowedOrigins = [
+      "https://hunterkf.com",
+      "https://speedruntravel.com",
+      "https://cooperpricehealth.vercel.app/",
+    ])
+  : (allowedOrigins = ["http://localhost:3000"]);
+
 // This function can be marked `async` if using `await` inside
 export async function middleware(req: NextRequest) {
-    // if (process.env.NODE_ENV !== "production") {
-    //     return NextResponse.next();
-    // }
-    // validate the headers with the api key
-    const xApiKey = req.headers.get("authorization");
+  // if (process.env.NODE_ENV !== "production") {
+  //     return NextResponse.next();
+  // }
 
-    if (xApiKey !== process.env.API_KEY) {
-        // cannot do {status: 401}
-        return NextResponse.json({ message: "Unauthorized" });
-    }
+  const { origin } = req.nextUrl;
 
-    return NextResponse.next();
+  // validate the headers with the api key
+  const xApiKey = req.headers.get("authorization");
+
+  if (xApiKey !== process.env.API_KEY && allowedOrigins.includes(origin)) {
+    // cannot do {status: 401}
+    return NextResponse.json({ message: "Unauthorized" });
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
-    matcher: [
-        "/api/article/:path*",
-        "/api/reel/:path*",
-        "/api/contact",
-        "/api/subscriber",
-        "/api/test",
-        "/api/booking",
-        "/api/booking/patient",
-    ],
+  matcher: [
+    "/api/article/:path*",
+    "/api/reel/:path*",
+    "/api/contact",
+    "/api/subscriber",
+    "/api/test",
+    "/api/booking",
+    "/api/booking/patient",
+  ],
 };
