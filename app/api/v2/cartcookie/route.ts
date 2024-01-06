d656cebdffce7f892b86087b10fb08a02421feb8
BestVersion7 <hunterkfan@gmail.com>
2024-01-05 21:06:28 -0500

    change prisma and middleware

--- app/api/v2/cartcookie/route.ts
+++ app/api/v2/cartcookie/route.ts
@@ -1,36 +1,42 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { cookies } from "next/headers";
 
-export async function POST() {
+export async function POST(req: Request) {
     try {
-        const data = await prisma.cookieInfo.create({});
-
-        const response = NextResponse.json("cookie created");
-        response.cookies.set({
-            domain: "localhost:3000",
-            name: "cookiecart",
-            value: data.cookie_id,
-            secure: true,
-            // httpOnly: true,
-            // max 4 days
-            maxAge: 60 * 60 * 24 * 4,
+        const { cookie_id } = await req.json();
+        await prisma.cookieInfo.create({
+            data: { cookie_id },
         });
+        // const response = NextResponse.json("cookie created");
+        // response.cookies.set({
+        //     name: "cookiecart",
+        //     value: data.cookie_id,
+        //     secure: true,
+        //     // httpOnly: true,
+        //     // max 4 days
+        //     maxAge: 60 * 60 * 24 * 4,
+        // });
 
-        return response;
+        return NextResponse.json("cookie added");
     } catch (error) {
-        return NextResponse.json("cookie fail", { status: 500 });
+        return NextResponse.json(error, { status: 500 });
     }
 }
 
-export async function DELETE() {
+export async function DELETE(req: NextRequest) {
     try {
-        const response = NextResponse.json("cookie deleted");
+        const cartCookie = req.nextUrl.searchParams.get("cookie_id");
         // cookies().delete("cookiecart");
-        const oneDay = 24 * 60 * 60 * 1000;
-        cookies().set("cookiecart", "value", { expires: Date.now() - oneDay });
+        // const oneDay = 24 * 60 * 60 * 1000;
+        // cookies().set("cookiecart", "value", { expires: Date.now() - oneDay });
+        await prisma.cookieInfo.delete({
+            where: {
+                cookie_id: cartCookie,
+            },
+        });
 
-        return response;
+        return NextResponse.json("cookie deleted");
     } catch (error) {
         return NextResponse.json("cookie fail", { status: 500 });
     }
--- middleware.ts
+++ middleware.ts
@@ -2,31 +2,32 @@ import { NextRequest, NextResponse } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(req: NextRequest) {
-  // if (process.env.NODE_ENV !== "production") {
-  //     return NextResponse.next();
-  // }
+    // if (process.env.NODE_ENV !== "production") {
+    //     return NextResponse.next();
+    // }
 
-  // validate the headers with the api key
-  const xApiKey = req.headers.get("authorization");
+    // validate the headers with the api key
+    const xApiKey = req.headers.get("authorization");
 
-  if (xApiKey !== process.env.API_KEY) {
-    // cannot do {status: 401}
-    return NextResponse.json({ message: "Unauthorized" });
-  }
+    if (xApiKey !== process.env.API_KEY) {
+        // cannot do {status: 401}
+        return NextResponse.json({ message: "Unauthorized" });
+    }
 
-  return NextResponse.next();
+    return NextResponse.next();
 }
 
 export const config = {
-  matcher: [
-    "/api/article/:path*",
-    "/api/reel/:path*",
-    "/api/contact",
-    "/api/subscriber",
-    "/api/test",
-    "/api/booking",
-    "/api/booking/patient",
-    "/api/v2/stripe/customer",
-    "/api/v2/stripe/invoice",
-  ],
+    matcher: [
+        "/api/article/:path*",
+        "/api/reel/:path*",
+        "/api/contact",
+        "/api/subscriber",
+        "/api/test",
+        "/api/booking",
+        "/api/booking/patient",
+        "/api/v2/stripe/customer",
+        "/api/v2/stripe/invoice",
+        "/api/v2/stripe/cartcookie",
+    ],
 };
