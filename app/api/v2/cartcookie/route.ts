a7bebd2a084e5b244c61a2b791da61d9e94a222e
BestVersion7 <hunterkfan@gmail.com>
2024-01-05 19:23:32 -0500

    change cartcookie to secure

--- app/api/v2/cartcookie/route.ts
+++ app/api/v2/cartcookie/route.ts
@@ -3,34 +3,34 @@ import { prisma } from "@/lib/prisma";
 import { cookies } from "next/headers";
 
 export async function POST() {
-  try {
-    const data = await prisma.cookieInfo.create({});
+    try {
+        const data = await prisma.cookieInfo.create({});
 
-    const response = NextResponse.json("cookie created");
-    response.cookies.set({
-      name: "cookiecart",
-      value: data.cookie_id,
-      secure: false,
-      // httpOnly: true,
-      // max 4 days
-      maxAge: 60 * 60 * 24 * 4,
-    });
+        const response = NextResponse.json("cookie created");
+        response.cookies.set({
+            name: "cookiecart",
+            value: data.cookie_id,
+            secure: true,
+            // httpOnly: true,
+            // max 4 days
+            maxAge: 60 * 60 * 24 * 4,
+        });
 
-    return response;
-  } catch (error) {
-    return NextResponse.json("cookie fail", { status: 500 });
-  }
+        return response;
+    } catch (error) {
+        return NextResponse.json("cookie fail", { status: 500 });
+    }
 }
 
 export async function DELETE() {
-  try {
-    const response = NextResponse.json("cookie deleted");
-    // cookies().delete("cookiecart");
-    const oneDay = 24 * 60 * 60 * 1000;
-    cookies().set("cookiecart", "value", { expires: Date.now() - oneDay });
+    try {
+        const response = NextResponse.json("cookie deleted");
+        // cookies().delete("cookiecart");
+        const oneDay = 24 * 60 * 60 * 1000;
+        cookies().set("cookiecart", "value", { expires: Date.now() - oneDay });
 
-    return response;
-  } catch (error) {
-    return NextResponse.json("cookie fail", { status: 500 });
-  }
+        return response;
+    } catch (error) {
+        return NextResponse.json("cookie fail", { status: 500 });
+    }
 }
