5e7dd526efadbedaa066983b4a775b3e3f641f9e
BestVersion7 <hunterkfan@gmail.com>
2024-01-05 22:11:45 -0500

    update get request q

--- app/api/v2/cart/total/route.ts
+++ app/api/v2/cart/total/route.ts
@@ -2,7 +2,7 @@ import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-    const { cartCookie } = await req.json();
+    const cartCookie = req.nextUrl.searchParams.get("cookie_id");
     try {
         // check for cookies
         if (!cartCookie) {
